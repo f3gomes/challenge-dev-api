b8b575f5b7002036850d51b5ed6afd918b25f8df
fomes <fgomesdeluna@gmail.com>
2024-04-06 10:17:12 -0300

    feat: creates db connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { main as connectDB } from "./config/database";
 
 const app: Application = express();
 const PORT = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({
